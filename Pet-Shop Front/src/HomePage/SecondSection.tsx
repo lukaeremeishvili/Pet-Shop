ec089685c40cd495c4cdc49a4d8ddb15b2f8a79a
Luka Eremeishvili <lukaeremeishvili06@gmail.com>
2025-02-13 14:53:25 +0400

    slider warning fix

--- Pet-Shop Front/src/HomePage/SecondSection.tsx
+++ Pet-Shop Front/src/HomePage/SecondSection.tsx
@@ -9,11 +9,9 @@ interface Animal {
   animal_description: string;
   isPopular: boolean;
 }
-
 interface ApiResponse {
   items: Animal[];
 }
-
 const SecondSection: React.FC = () => {
   const [popularAnimals, setPopularAnimals] = useState<Animal[]>([]);
 
@@ -47,7 +45,7 @@ const SecondSection: React.FC = () => {
   }, []);
 
   return (
-    <section className="section2">
+    <section className="section2 mb-16">
       <h1 className="text-3xl font-semibold text-black leading-[54px] tracking-[0%] text-center">
         Popular Animals
       </h1>
@@ -55,44 +53,49 @@ const SecondSection: React.FC = () => {
         Top Picks
       </p>
       <div className="max-w-[867px] mx-auto">
-        <Swiper
-          loop={true}
-          slidesPerView={3}
-          slidesPerGroup={3}
-          centeredSlides={false}
-          spaceBetween={0}
-          grabCursor={true}
-          keyboard={{ enabled: true }}
-          scrollbar={{ hide: false }}
-          navigation={true}
-          pagination={{ clickable: true }}
-          modules={[Keyboard, Scrollbar, Navigation, Pagination]}
-          className="mySwiper"
-        >
-          {popularAnimals.map((animal) => {
-            const imageUrl = new URL(
-              `../assets/${animal.image}`,
-              import.meta.url
-            ).href;
+        {popularAnimals.length > 0 && (
+          <Swiper
+            loop={true}
+            slidesPerView={3}
+            slidesPerGroup={1}
+            centeredSlides={false}
+            spaceBetween={0}
+            grabCursor={true}
+            keyboard={{ enabled: true }}
+            scrollbar={{ hide: false }}
+            navigation={true}
+            pagination={{ clickable: true }}
+            modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+            className="mySwiper"
+          >
+            {popularAnimals.map((animal) => {
+              const imageUrl = new URL(
+                `../assets/${animal.image}`,
+                import.meta.url
+              ).href;
 
-            return (
-              <SwiperSlide key={animal._uuid}>
-                <div className="w-[237px] h-[326px] rounded-lg overflow-hidden bg-[#FBFBFB] shadow-[0px_10px_20px_0px_#0000001A]">
-                  <img
-                    src={imageUrl}
-                    alt={animal.name}
-                    className="w-full h-48 object-cover rounded-bl-2xl rounded-br-2xl"
-                  />
-                  <div className="p-4">
-                    <h1 className="font-poppins font-medium text-[16px] leading-[24px] tracking-[0%] text-center text-black mt-3">
-                      {animal.name}: {animal.animal_description}
-                    </h1>
+              return (
+                <SwiperSlide className="pb-5" key={animal._uuid}>
+                  <div className="w-[237px] h-[400px] flex flex-col rounded-lg overflow-hidden bg-[#FBFBFB] shadow-[0px_10px_20px_0px_#0000001A]">
+                    <img
+                      src={imageUrl}
+                      alt={animal.name}
+                      className="w-full h-48 object-cover rounded-bl-2xl rounded-br-2xl"
+                    />
+                    <div className="p-4 flex flex-col justify-between">
+                      <h1 className="font-poppins font-medium text-[16px] leading-[24px] tracking-[0%] text-center text-black mt-3">
+                        {animal.name}
+                      </h1>
+                      <p className="font-poppins font-normal text-[14px] leading-[20px] text-center text-black mt-3 overflow-hidden text-ellipsis h-[100px]">
+                        {animal.animal_description}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        )}
       </div>
     </section>
   );
