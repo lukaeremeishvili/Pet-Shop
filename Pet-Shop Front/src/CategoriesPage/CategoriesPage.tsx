c1ef97b878275d171e326d2060307806ee0fdbde
Luka Eremeishvili <lukaeremeishvili06@gmail.com>
2025-02-14 10:24:49 +0400

    adjusted categories page title position

--- Pet-Shop Front/src/CategoriesPage/CategoriesPage.tsx
+++ Pet-Shop Front/src/CategoriesPage/CategoriesPage.tsx
@@ -111,7 +111,7 @@ const CategoriesPage = () => {
       ) : (
         categories.map((categoryData) => (
           <div key={categoryData.category} className="mb-8">
-            <h2 className="text-xl font-semibold mb-4">
+            <h2 className="text-xl font-semibold mb-4 text-center">
               {categoryData.category}
             </h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
